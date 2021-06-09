b8289e741123d1b81ace341f68453f9ae2f19886
cybercris <crssoares.js@gmail.com>
2021-06-08 21:35:39 -0300

    useState hook to add/catch user in api call/response using axios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
+  const [newUser, setNewUser] = useState('');
+  const [users, setUsers] = useState([]);
+
+  async function handleAddUser() {
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    setUsers([...users, data]);
+    setNewUser('');
+
+    Keyboard.dismiss();
+  }
+
   return (
     <Container>
       <Form>
@@ -11,8 +32,12 @@ export default function Main() {
           autoCorrect={false}
           autoCapitalize="none"
           placeholder="Adicionar usuário"
+          value={newUser}
+          onChangeText={(newUser) => setNewUser(newUser)}
+          returnKeyType="send"
+          onSubmitEditing={handleAddUser}
         />
-        <SubmitButton>
+        <SubmitButton onPress={handleAddUser}>
           <Icon name="add" size={20} color="#fff" />
         </SubmitButton>
       </Form>
